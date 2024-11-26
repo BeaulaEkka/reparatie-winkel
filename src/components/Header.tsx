c5e423379e1c498cd4aa15d76dc14e1bfd28e835
Your Name <ekka.beaula@gmail.com>
2024-11-26 22:17:38 +0100

    created theme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,31 @@
 import React from "react";
 import { HomeIcon, File, UserRound } from "lucide-react";
+import NavButton from "./NavButton";
+import Link from "next/link";
+import { ModeToggle, Toggle } from "./ModeToggle";
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="animate-slide bg-background h-12 p-2 border-b sticky top-0 z-20">
+      <div className="flex h-8 items-center justify-between w-full">
+        {" "}
+        <div className="flex items-center gap-2">
+          <NavButton href="/home" label="home" icon={HomeIcon} />
+          <Link
+            className="flex justify-center items-center gap-2 ml-0"
+            title="home"
+            href="/home"
+          >
+            <h1 className="hidden sm:block text-xl font-bold mt-1 m-0">
+              Computer Repair Shop
+            </h1>
+          </Link>
+        </div>
+        <div className="flex items-center">
+          <NavButton href="/tickets" label="Tickets" icon={File} />
+          <NavButton href="/customers" label="Customers" icon={UserRound} />
+          <ModeToggle />
+        </div>
+      </div>
+    </header>
+  );
 }
--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "./ui/button";
-import { Link, LucideIcon } from "lucide-react";
+import { LucideIcon } from "lucide-react";
+import Link from "next/link";
 
 type Props = {
   icon: LucideIcon;
